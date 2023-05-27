8ba42d9d85bd282bac8ede48c3ccdd9ac8fcac39
Brent Reynolds <brentsreynolds5@gmail.com>
2023-05-27 19:37:32 -0400

    Init

---
+++ rock_paper_scissors.py
@@ -0,0 +1,13 @@
+def get_choices():
+    player_choice = "rock"
+    computer_choice = "paper"
+    choices = {"player": player_choice, "computer": computer_choice}
+    return choices
+
+def greeting():
+    return "Hi"
+
+choices = get_choices()
+print(choices)
+
+dict = {"name": "Brent", "color": "blue"}
