6b08608281d1e6fdfa10f3a3f11f42d118625484
Brent Reynolds <brentsreynolds5@gmail.com>
2023-05-27 22:41:03 -0400

    Enabled player and computer to play

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -1,12 +1,34 @@
+import random
+
 def get_choices():
-    player_choice = "rock"
-    computer_choice = "paper"
+    player_choice = input("Enter a choice (rock, paper, scissors): ")
+    options = ["rock", "paper", "scissors"]
+    computer_choice = random.choice(options)
     choices = {"player": player_choice, "computer": computer_choice}
     return choices
 
-def greeting():
-    return "Hi"
-
+def check_win(player, computer):
+    print(f"You chose {player}, computer chose {computer}")
+    if player == computer:
+        return "It's a tie!"
+    elif player == "rock":
+       if computer == "scissors":
+        return "Rock smashes scissors! You win!"
+       else: 
+        return "Paper covers rock! You lose."
+       
+    elif player == "paper":
+       if computer == "rock":
+        return "Paper covers rock! You win!"
+       else: 
+        return "Scissors cuts paper! You lose."
+       
+    elif player == "scissors":
+       if computer == "paper":
+        return "Scissors cuts paper! You win!"
+       else: 
+        return "Rock smashes scissors! You lose!"
+    
 choices = get_choices()
-print(choices)
-
+result = check_win(choices["player"], choices["computer"])
+print(result)
